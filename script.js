a6be46a1663ba8df350e75cbca3e94ff6200c25b
jmisacube <ok.j.pepe@gmail.com>
2021-03-24 10:34:14 -0700

    Danny Luong's story added

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ let postsArray = [
 //[ "posts/2020-11-10-Special-Characters-Example.html", encodeURI( 'Spéci@l "Character\'s" Examp|e' ) ]
 //[ "posts/2020-11-10-My-Third-Post-Example.html" ],
 //[ "posts/2020-11-10-My-Second-Post-Example.html" ],
+[ "posts/2021-03-24-Departing.html", encodeURI( 'Departing...' ) ],
 [ "posts/2021-03-17-Kevin-Ko.html" ] ];
 
 //-----------------------------
